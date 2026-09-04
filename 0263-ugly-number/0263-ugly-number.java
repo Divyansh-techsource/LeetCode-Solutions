91e086b9a5b5a7ff106a63e23003f0c49fe25d94
Divyansh-techsource <dk.tech2007@gmail.com>
2026-09-04 11:45:55 +0530

    Time: 1 ms (99.02%), Space: 42.6 MB (32.11%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isUgly(int n) {
-        if (n <= 0)
+        if (n < 1) {
             return false;
-        int[] factors = { 2, 3, 5 };
-        for (int fact : factors) {
-            while (n % fact == 0) {
-                n /= fact;
-            }
+        }
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
         }
         return n == 1;
     }
