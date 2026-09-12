62206cec8ba5fc38ea464bef17f9a2da09947c31
Divyansh-techsource <dk.tech2007@gmail.com>
2026-09-12 18:06:20 +0530

    Time: 1 ms (94.96%), Space: 42.2 MB (40.25%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,47 +1,44 @@
 class Solution {
+    int count = 0;
+    int n;
 
-    static int N;
-    static int[][] board;
-    static int count;
-
-    static boolean isSafe(int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 1) {
-                return false;
-            }
-        }
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 1) {
-                return false;
-            }
-        }
-        for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
-            if (board[i][j] == 1) {
-                return false;
-            }
-        }
-        return true;
+    public int totalNQueens(int n) {
+        this.n = n;
+        boolean rowUsed[] = new boolean[n];
+        boolean colUsed[] = new boolean[n];
+        boolean leftDiagUsed[] = new boolean[2 * n - 1];
+        boolean rightDiagUsed[] = new boolean[2 * n - 1];
+        solve(0, rowUsed, colUsed, leftDiagUsed, rightDiagUsed);
+        return count;
     }
 
-    static void solve(int row) {
-        if (row == N) {
+    void solve(int r, boolean[] row, boolean[] col, boolean[] leftDiag, boolean[] rightDiag) {
+        if (r == n) {
             count++;
             return;
         }
-        for (int col = 0; col < N; col++) {
-            if (isSafe(row, col)) {
-                board[row][col] = 1;
-                solve(row + 1);
-                board[row][col] = 0;
+        for (int c = 0; c < n; c++) {
+            if (row[r] == true) {
+                continue;
             }
+            if (col[c] == true) {
+                continue;
+            }
+            if (rightDiag[r + c] == true) {
+                continue;
+            }
+            if (leftDiag[r - c + n - 1] == true) {
+                continue;
+            }
+            row[r] = true;
+            col[c] = true;
+            leftDiag[r - c + n - 1] = true;
+            rightDiag[r + c] = true;
+            solve(r + 1, row, col, leftDiag, rightDiag);
+            row[r] = false;
+            col[c] = false;
+            leftDiag[r - c + n - 1] = false;
+            rightDiag[r + c] = false;
         }
-    }
-
-    public int totalNQueens(int n) {
-        N = n;
-        board = new int[N][N];
-        count = 0;
-        solve(0);
-        return count;
     }
 }
